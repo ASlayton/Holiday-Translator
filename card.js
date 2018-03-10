e64e33514c28826db3f53e8fe6ca8e42642f8964
Amanda Slayton <alslayton86@gmail.com>
2018-03-10 17:18:30 -0600

    added a randomly generated card feature

---
+++ card.js
@@ -0,0 +1,14 @@
+var myBackgrounds=["/images/cartoon-christmas-lights-border.jpg","/images/cartoon-christmas-lights-border.jpg", "/images/christmas-border-3044685_960_720.png", "/images/christmas-borders-3016691_960_720.png", "/images/simages(1).jpg", "/images/images.jpg"];
+// "url('img_tree.png')"
+
+function randomNumber(){
+  var myNumber = Math.floor((Math.random() * (myBackgrounds.length-1) + 1));
+  return myNumber;
+};
+
+function generateACard(){
+  document.getElementById("generatedCard").style.backgroundImage = "url(" + myBackgrounds[randomNumber()] + ")";
+
+  document.getElementById("insertUserTextHere").innerHTML = document.getElementById("translatedText").value;
+  document.getElementById("generatedCard").style.visibility = 'visible';
+};
