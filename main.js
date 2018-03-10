8e10e695bd499cd8ffb70b10b07706e92d8a470c
Amanda Slayton <alslayton86@gmail.com>
2018-03-10 11:53:46 -0600

    added language object

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("Why, hello there.")
+
+var languageObject =
+{
+  happy:     ["feliz", "glücklich", "feliĉa", "quch"],
+  merry:     ["alegre", "fröhlich", "gaja", "quch"],
+  new:       ["nuevo", "neu", "nova", "chu\'"],
+  christmas: ["navidad", "weihnachten", "kristnasko", "QISmaS"],
+  year:      ["ano", "jahr", "jaro", "DIS"],
+  you:       ["tu", "sie", "vi", "SoH"],
+  hope:      ["esperanza", "hoffnung", "espero", "tul"],
+  i:	       ["yo", "ich", "i", "jIH"],
+  and:       ["y", "und", "kaj", "je"],
+  hanukah:   ["hanukkah", "hanukkah", "hanukkah", "hanukah"],
+  kwanzaa:   ["kwanzaa", "kwanzaa", "kwanzaa", "kwanzaa"],
+  jingle:    ["tintineo", "klimpern", "enreta", "jingle"],
+  bells:     ["campanas", "glocken", "sonoriloj", "\'In"],
+  deck:      ["cubierta", "deck", "ferdeko", "choQ"],
+  the:       ["el", "das", "la", "."],
+  halls:     ["pasillos", "hallen", "salonoj", "vaS"],
+  wish:      ["deseo", "wunsch", "deziras", "vIneH"],
+  joy:       ["alegría", "freude", "ĝojo", "yImaq"],
+  of:        ["de", "von", "deziras", ","],
+  holly:     ["acebo", "stechpalme", "hipo", "holly"],
+  any:       ["muchos", "viele", "multaj", "vay\'"],
+  returns:   ["devoluciones", "kehrt zurück", "revenas", "nobHa\'"],
+  seasons:   ["estaciones", "jahreszeiten", "sezonoj", "maqtagh"],
+  greetings: ["saludos", "schöne grüße", "salutoj", "ghaH \'ej Duvan"],
+  holiday:   ["fiesta", "urlaub", "feriado", "QI\'lop"],
+  wishes:    ["deseos", "wünscht sich", "deziras", "vIneH"],
+  greeting:  ["saludo", "grüß", "saluto", "ghaH 'ej Duvan"],
+  love:      ["amor", "liebe", "amo", "parmaq"],
+  let:       ["dejar", "lassen", "lasu", "yInISQo\'"],
+  it:	       ["eso", "es", "gi", "\'oH"],
+  snow:      ["nieve", "schnee", "nego", "peD"]
+};
