a82edfa7f7d495fc4c3dd5c07a30004edaf81503
Amanda Slayton <alslayton86@gmail.com>
2018-03-11 15:23:11 -0500

    added Feeling Lucky button

--- card.js
+++ card.js
@@ -1,8 +1,8 @@
-var myBackgrounds=["/images/cartoon-christmas-lights-border.jpg","/images/cartoon-christmas-lights-border.jpg", "/images/christmas-border-3044685_960_720.png", "/images/christmas-borders-3016691_960_720.png", "/images/simages(1).jpg", "/images/images.jpg"];
+var myBackgrounds=["/images/cartoon-christmas-lights-border.jpg","/images/cartoon-christmas-lights-border.jpg", "/images/christmas-border-3044685_960_720.png", "/images/christmas-borders-3016691_960_720.png", "/images/images(1).jpg", "/images/images.jpg"];
 // "url('img_tree.png')"
 
 function randomNumber(){
-  var myNumber = Math.floor((Math.random() * (myBackgrounds.length-1) + 1));
+  var myNumber = Math.floor((Math.random() * (myBackgrounds.length-1)));
   return myNumber;
 };
 
